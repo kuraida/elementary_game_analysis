59fa909f64907c0a4f9b7061a69c286ede841ad2
Chris <kuraida@localhost.localdomain>
2021-01-12 15:13:34 -0600

    begin constructing final giantbomb queries

--- gb_request.py
+++ gb_request.py
@@ -10,23 +10,44 @@ headers = {'user-agent':user_info['user-agent']} #giant bomb api rejects request
 
 
 payload = {
+
+
+    
     'api_key':user_info['api_key'],
     'format':'json',
-    'field_list':'name,original_release_date',
-    'sort':'',
-    'filter':'',
-    'limit':'5'
+    'field_list':'guid,id,name'
+
+}
+
+games_query = {
+
+    'api_key':user_info['api_key'],
+    'format':'json',
+    'field_list':'guid,id,name,platforms,deck,original_release_date,original_game_rating',
+    'limit':'10',
+    'sort':'name',
+    'filter':'platforms:19'
 
 }
 
 
-r = requests.get('https://www.giantbomb.com/api/games/', headers=headers, params=payload)
 
 
 
-data = json.loads(r.text)
 
-print(data)
+
+games_request = requests.get('https://www.giantbomb.com/api/games/', headers=headers, params=games_query)
+
+
+#platform_request = requests.get('https://www.giantbomb.com/api/platforms/', headers=headers, params=payload)
+
+
+results = pd.DataFrame(json.loads(games_request.text))
+data_df = pd.DataFrame(json.loads(games_request.text)['results'])
+data_df.to_csv('./data/games_sample.csv')
+
+print(results)
+print(data_df)
 # with open('./data/gb_test.json', 'w') as out_file:
 #     json.dump(json.loads(r.text), out_file)
 #gb_df = pd.DataFrame(json.loads(r.text))
