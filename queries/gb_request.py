8afe12bee5e0e10b7f3ebb8ea8ecfe0d4c84e7e5
Chris <kuraida@localhost.localdomain>
2021-01-13 10:33:01 -0600

    fix games query fields filter, generalize code for multiple platforms

--- queries/gb_request.py
+++ queries/gb_request.py
@@ -9,18 +9,19 @@ import time
 user_info = json.loads(open('./auth.json').read())
 headers = {'user-agent':user_info['user-agent']} #giant bomb api rejects requests without a unique user-agent
 
+
 #load in platforms, create api filter based on platforms used
 platforms = pd.read_csv('./data/platforms.csv')
 platform_filter='platforms:'
 for p in platforms.sort_values('id')['id']:
     platform_filter = platform_filter + str(p) + '|'
 
-#determine the total number of games we will be querying
 
+#determine the total number of games we will be querying
 touch_params = {
     'api_key':user_info['api_key'],
     'format':'json',
-    'filter':'platforms:19',
+    'filter':platform_filter,
     'limit':1
 }
 
@@ -34,29 +35,33 @@ n = json.loads(touch_request.text)['number_of_total_results']
 games_params = {
     'api_key':user_info['api_key'],
     'format':'json',
-    'fields_list':'guid,id,name,platforms,description,number_of_user_reviews,original_game_rating,original_release_date',
-    'filter':'platforms:19',
-    'limit':'100'
+    'field_list':'guid,id,name,platforms,description,number_of_user_reviews,original_game_rating,original_release_date',
+    'filter':platform_filter,
+    'limit':'100',
+    'sort':'id'
 }
 
+
+games_df = pd.DataFrame(columns=games_params['fields_list'].split(',')) #output dataframe
+
 idx = 0
-games_df = pd.DataFrame(columns=games_params['fields_list'].split(','))
-print(games_df.head())
 while idx <= n:
 
     games_params['offset'] = str(idx)
     games_request = requests.get('https://giantbomb.com/api/games/', headers=headers, params=games_params)
     result_df = pd.DataFrame(json.loads(games_request.text)['results'])
     games_df = games_df.append(result_df)
+    
     idx += 100
-    print(idx)
-    time.sleep(18)
+    
+    print('current progress: index=' + idx + ' of ' + n + ' total')
+    time.sleep(18) #space out requests as to not violate GiantBomb's rate limits
 
 
 print(games_df)
 print(games_df.shape)
 
-games_df.to_csv('./data/ps2.csv')
+games_df.to_csv('./data/games.csv')
     
 
 
