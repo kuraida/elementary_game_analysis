e517c2269b586e11469aef5834d0977c281b810e
CHRIS CRIDER <kuraida.cc@gmail.com>
2021-02-16 13:23:17 -0600

    restructure game details query

--- queries/gb_game_details_request.py
+++ queries/gb_game_details_request.py
@@ -18,15 +18,14 @@ query_params = {
     'api_key':user_info['api_key'],
     'format':'json',
     'field_list':'',
-    'filter':platform_filter,
+    'filter': '',
     'limit':'100',
     'sort':'guid'
 }
 
-games_details_df = pd.DataFrame(columns='')
+games_details_df = pd.DataFrame(columns=[])
 
 query_idx = 0
-
 while query_idx < games_df.shape[0]
 
     #fetch details for 100 games, iterate idx
@@ -34,11 +33,10 @@ while query_idx < games_df.shape[0]
     
     guid_filter = games_df[query_idx, query_idx+100]['guid'].to_string(index=False)
     guid_filter = 'guid:' + guid_filter.replace('\n', '|')
+    query_params['filter'] = guid_filter
 
     games_request = requests.get('https://giantbomb.com/api/games/', headers=headers, params=games_params)
-
-
-
+    
 
     time.sleep(18) #space out requests so as not to violate GiantBomb's rate limits
     query_idx += 100
