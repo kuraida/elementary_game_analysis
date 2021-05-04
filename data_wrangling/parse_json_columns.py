2e293df107883534bf170172ba44cb8b79d4dc95
Chris Crider <chris@localhost.localdomain>
2021-05-04 12:41:57 -0500

    finish json parsing script

--- data_wrangling/parse_json_columns.py
+++ data_wrangling/parse_json_columns.py
@@ -8,10 +8,10 @@ import pandas as pd
 
 
 Many of the fields in the GiantBomb API query results are formatted as json references to separate API resources. Since querying the full content of those resources is beyond the scope of this project and would constitute undue strain
-on GiantBomb's api, here we will simply pull relevant details out of the json string and export them to their own csvs. Because these will come from the data we have queried for this project, this will not be a complete representation
+on GiantBomb's api, here we will simply pull relevant details out of the json string and export them to their own .csvs. Because these will come from the data we have queried for this project, this will not be a complete representation
 of the data hosted on GiantBomb, but rather a catalog of the field values appearing in this project's data.
 
-Here we will identify the columns we want to parse, pull out the relevant info from the json objects, and replace the values in the source dataframe/csv/table with the id reference to cut down on redundancy.
+Here we will identify the columns we want to parse, convert the json strings to dicts, pull out the relevant info, and replace the values in the source dataframe/csv/table with the id reference to cut down on redundancy.
 
 '''
 
@@ -21,23 +21,25 @@ Here we will identify the columns we want to parse, pull out the relevant info f
 def parse_json(in_df, fields):
 
     #games details - future goal is to make this function generic so that it can be run against all api query csvs
-    for field in fields:
+    for field in fields: #columns from data source that are formatted as api references
+        
         out_df = pd.DataFrame()
-        for idx in in_df.index:
+        for idx in in_df.index: #needs to be run for each record to capture all unique values for each field
 
             item_list = []
             
             json_string = in_df.loc[idx][field]
-            json_eval = ast.literal_eval(json_string)
+            json_eval = ast.literal_eval(json_string) #returns json object as python dict
 
+            #replace api references in source data with python list of unique ids,  append record dicts to output dataframe
             for item in json_eval:
                 item_list.append(item['id'])
                 out_df = out_df.append(item, ignore_index=True)
             
-            print(item_list)
             in_df.at[idx, field] = item_list
 
-        out_df.to_csv('./data/' + field + '.csv', index=False)
+       
+        out_df.drop_duplicates().to_csv('./data/' + field + '.csv', index=False) #only export unique records
         
 
     return in_df
@@ -48,9 +50,8 @@ def parse_json(in_df, fields):
 
 def main():
     
-    input_df = pd.read_csv('./data/game_details.csv')[:20]
+    input_df = pd.read_csv('./data/game_details.csv')[:100]
     field_list = ['genres']
     parsed_df = parse_json(input_df, ['genres'])
-    print(parsed_df['genres'].head())
 
 main()
