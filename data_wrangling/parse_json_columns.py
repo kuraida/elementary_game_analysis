e33cbb0103e3d112b5dce2ea251ce59d4f4fac36
Chris Crider <chris@localhost.localdomain>
2021-05-04 11:46:16 -0500

    build json parsing function, export no-build yml

---
+++ data_wrangling/parse_json_columns.py
@@ -0,0 +1,56 @@
+import ast 
+import pandas as pd
+
+
+'''
+***THIS IS A PRELIMINARY SCRIPT - AT A LATER DATE THIS SECTION OF THE PROJECT WILL OCCUPY ITS OWN JUPYTER NOTEBOOK***
+
+
+
+Many of the fields in the GiantBomb API query results are formatted as json references to separate API resources. Since querying the full content of those resources is beyond the scope of this project and would constitute undue strain
+on GiantBomb's api, here we will simply pull relevant details out of the json string and export them to their own csvs. Because these will come from the data we have queried for this project, this will not be a complete representation
+of the data hosted on GiantBomb, but rather a catalog of the field values appearing in this project's data.
+
+Here we will identify the columns we want to parse, pull out the relevant info from the json objects, and replace the values in the source dataframe/csv/table with the id reference to cut down on redundancy.
+
+'''
+
+
+
+
+def parse_json(in_df, fields):
+
+    #games details - future goal is to make this function generic so that it can be run against all api query csvs
+    for field in fields:
+        out_df = pd.DataFrame()
+        for idx in in_df.index:
+
+            item_list = []
+            
+            json_string = in_df.loc[idx][field]
+            json_eval = ast.literal_eval(json_string)
+
+            for item in json_eval:
+                item_list.append(item['id'])
+                out_df = out_df.append(item, ignore_index=True)
+            
+            print(item_list)
+            in_df.at[idx, field] = item_list
+
+        out_df.to_csv('./data/' + field + '.csv', index=False)
+        
+
+    return in_df
+
+
+
+
+
+def main():
+    
+    input_df = pd.read_csv('./data/game_details.csv')[:20]
+    field_list = ['genres']
+    parsed_df = parse_json(input_df, ['genres'])
+    print(parsed_df['genres'].head())
+
+main()
